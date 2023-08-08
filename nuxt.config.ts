65a3f828be5ebe1ef26bbef5d3006d7c12683262
Emily H <emilyyho33@gmail.com>
2023-08-08 12:59:02 -0400

    add vercel config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,10 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
+  routeRules: {
+    '/examples/*': { redirect: '/redirect-route' },
+    '/modify-headers-route': { headers: { 'x-magic-of': 'nuxt and vercel' } },
+    '/spa': { ssr: false },
+  },
 })
 
